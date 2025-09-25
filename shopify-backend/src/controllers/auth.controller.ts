898094b46035131a2dc76560b351a263b3bc53a0
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-09-26 01:42:12 +0530

    feat : added auth-controllers

---
+++ shopify-backend/src/controllers/auth.controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { asyncHandler } from "../utils/asyncHandler";
+import * as authservice from "../services/auth.services";
+import { loginSchema, signupSchema } from "../validators/validation";
+import { ApiError } from "../utils/apiError";
+
+export const signup = asyncHandler(async (req: Request, res: Response) => {
+  const body = signupSchema.safeParse(req.body);
+  if (!body.success) throw ApiError.badRequest("Invalid credentials");
+
+  const { email, password, role } = req.body;
+
+  const user = await authservice.signup(email, password, role);
+
+  return res.status(201).json({
+    id: user.id,
+    email: user.email,
+    role: user.role,
+  });
+});
+
+export const login = asyncHandler(async (req: Request, res: Response) => {
+  const body = loginSchema.safeParse(req.body);
+  if (!body.success) throw ApiError.badRequest("Invalid credentials");
+
+  const { email, password } = req.body;
+
+  const payload = await authservice.login(email, password);
+
+  return res.json(payload);
+});
