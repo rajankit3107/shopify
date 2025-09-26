dea55506a4fcacf28578c384ac473484326c5250
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-09-26 15:44:43 +0530

    feat : added vendor services

---
+++ shopify-backend/src/services/vendor.services.ts
@@ -0,0 +1,41 @@
+import prisma from "../prismaClient";
+import { ApiError } from "../utils/apiError";
+
+export async function createVendor(
+  ownerId: string,
+  payload: {
+    name: string;
+    slug: string;
+    description?: string;
+    logoUrl?: string;
+  }
+) {
+  const existing = await prisma.vendor.findUnique({
+    where: {
+      slug: payload.slug,
+    },
+  });
+
+  if (existing)
+    throw ApiError.badRequest("vendor with this slug already exists");
+
+  const vendor = await prisma.vendor.create({
+    data: {
+      ...payload,
+      ownerId,
+    },
+  });
+
+  return vendor;
+}
+
+export async function getBySlug(slug: string) {
+  return prisma.vendor.findUnique({
+    where: { slug },
+    include: { products: true },
+  });
+}
+
+export async function listAll() {
+  return prisma.vendor.findMany({ include: { products: true } });
+}
