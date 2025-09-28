2532a389d2d3c3397e448081789d4f3d895b71ac
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-09-28 17:45:42 +0530

    fix : fixed prod bugs

--- shopify-backend/src/index.ts
+++ shopify-backend/src/index.ts
@@ -16,8 +16,11 @@ app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/vendors", vendorRoutes);
