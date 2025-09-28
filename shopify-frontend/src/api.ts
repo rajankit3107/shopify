f67ca1cc2bd4b9f1fffd384ffb764a15f9843cfa
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-09-28 22:46:38 +0530

    fix : fixed prod bugs

--- shopify-frontend/src/api.ts
+++ shopify-frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE ?? "http://localhost:3000/api";
+  import.meta.env.VITE_API_BASE;
 
 const client = axios.create({ baseURL: API_BASE_URL });
 
